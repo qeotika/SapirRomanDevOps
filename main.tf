88cde91fec64063480c16366117103553235111c
Roman Dyachenko <r0dsen15@gmail.com>
2022-11-02 21:24:45 +0200

    VPC updated for AWS

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
   region = "eu-west-3"
 }
 resource "aws_vpc" "Roman-dev-vpc" {
-  cidr_block = "192.0.2.0/27"
+  cidr_block = "192.0.2.0/24"
   tags = {"Name" = "Roman-dev-vpc"}
   
 }
@@ -18,5 +18,18 @@ resource "aws_subnet" "Roman-dev-subnet" {
   vpc_id = aws_vpc.Roman-dev-vpc.id
   cidr_block = "192.0.2.0/27"
   tags = {"Name" = "Roman-k8s-subnet"}
+  
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id=aws_vpc.Roman-dev-vpc.id
+  tags={
+    Name= "RomanIgw"
+  }
+  
+}
+resource "aws_route" "routeIGW" {
+  route_table_id = aws_vpc.Roman-dev-vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
   
 }
